c6870f768f0f742bf2b7cd8f9bb71aac2807c4d4
Eli Kin <3limkin@gmail.com>
2020-10-03 15:29:03 -0400

    Added different font options

--- postcard.js
+++ postcard.js
@@ -7,12 +7,13 @@ function updateCanvas() {
     var text = document.getElementById("text");
     var textColor = document.getElementById("textColor");
     var textSize = document.getElementById("textSize");
+    var textFont = document.getElementById("textFont");
 
     if (canvasImage.value == "") {
         ctx.fillStyle = canvasColor.value;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = textColor.value;
-        ctx.font = textSize.value + "px Arial";
+        ctx.font = textSize.value + "px " + textFont.value;
         fillText(ctx, text.value, parseInt(textSize.value), 2*parseInt(textSize.value));
     }
     else {
@@ -21,7 +22,7 @@ function updateCanvas() {
         img.onload = function() {
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
             ctx.fillStyle = textColor.value;
-            ctx.font = textSize.value + "px Arial";
+            ctx.font = textSize.value + "px " + textFont.value;
             fillText(ctx, text.value, parseInt(textSize.value), 2*parseInt(textSize.value));
         }
     }
