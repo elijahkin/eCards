c9eebca0780aa24246069ae3af80ca622a2bbd9a
Eli Kin <3limkin@gmail.com>
2020-10-03 10:58:13 -0400

    Added clear canvas and text size controls

--- postcard.js
+++ postcard.js
@@ -1,17 +1,20 @@
-function colorChange() {
+function updateCanvas() {
     var canvas = document.getElementById("canvas");
     var color = document.getElementById("color");
+    var text = document.getElementById("text");
+    var textSize = document.getElementById("textSize");
     var ctx = canvas.getContext("2d");
+
     ctx.fillStyle = color.value;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function textChange() {
-    var canvas = document.getElementById("canvas");
-    var text = document.getElementById("text");
-    var ctx = canvas.getContext("2d");
 
     ctx.fillStyle = "black";
-    ctx.font = "30px Arial";
+    ctx.font = textSize.value + "px Arial";
     ctx.fillText(text.value, 10, 50);
+}
+
+function clearCanvas() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
